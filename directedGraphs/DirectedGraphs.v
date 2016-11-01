9b389d765c319b96acb31c1a54cfc281d8df0b6c
ns196414 <ns196414@ohio.edu>
2016-11-01 16:07:04 -0400

    Added an invariant to my graph

--- directedGraphs/DirectedGraphs.v
+++ directedGraphs/DirectedGraphs.v
@@ -142,25 +142,43 @@ Module myGraph : DirectedGraphs.
 
 
 
-  Definition edge := Edges.t.
+  Definition edge := Edges.elt.
   Definition v_set := Vertices.t.
   Definition e_set := Edges.t.
+  Inductive ok : v_set -> e_set -> Prop :=
+  | emp_ok : ok Vertices.empty Edges.empty
+  | other : forall (V : v_set) (E : e_set) (e : edge),  (Vertices.In (fst e) V) /\ (Vertices.In (snd e) V) -> ok V E.
+
+
   Record Graph :=
     mkgraph {
         V : v_set;
         E : e_set;
+        o : ok V E
       }.
 
   Definition t := Graph.
-
-  Definition empty := mkgraph Vertices.empty Edges.empty.
-  Open Scope positive_scope.
-
+  
   Notation vertex := Vertices.elt.
-    
-
+  
   Definition buildEdge (n n1 : vertex): Edges.elt :=
     (n,n1).
+
+  (*   Notation "[ ]" := Edges.empty. *)
+
+  (* Notation "[ elt0 , .. , eltn ]" := (Edges.add elt0 .. (Edges.add eltn (Edges.empty )) .. ) (at level 60, right associativity). *)
+
+
+  Open Scope positive_scope.
+
+
+
+  Definition empty := mkgraph Vertices.empty Edges.empty emp_ok.
+
+
+
+    
+
 
 
   (* Definition destructEdge : edge -> (vertex * vertex) := *)
@@ -191,38 +209,38 @@ Definition destructEdge : Edge.t -> (vertex * vertex) :=
   Definition  IsEmpty : Graph -> Prop :=
     fun X : Graph =>
       match X with
-      | {| V := V0; E := e0 |} => Vertices.Empty V0  /\ Edges.Empty e0
+      | {| V := V0; E := e0; ok := p |} => Vertices.Empty V0  /\ Edges.Empty e0
       end.
 
   Definition IsVertex : Vertices.elt -> Graph -> Prop :=
     fun (n : Vertices.elt) (g : Graph)  =>
       match g with
-      | {| V:= v; E := e0 |} => Vertices.In n v
+      | {| V:= v; E := e0; ok := p |} => Vertices.In n v
       end.
 
   Definition IsEdge : Edges.elt -> Graph -> Prop :=
     fun (e : Edges.elt) (g : Graph) =>
       match g with
-      | {| V := v; E := e0 |} => Edges.In e e0
+      | {| V := v; E := e0; ok := p |} => Edges.In e e0
       end.
   
     Definition isEmpty : Graph -> bool :=
       fun (g : Graph) =>
         match g with
-        | {| V := v; E := e |} =>
+        | {| V := v; E := e; ok := p |} =>
           andb (Vertices.is_empty v) (Edges.is_empty e)
         end.
 
     Definition isVertex : Vertices.elt -> Graph -> bool :=
       fun (n : Vertices.elt) (g : Graph)  =>
         match g with
-        | {| V:= v; E := e0 |} => Vertices.mem n v
+        | {| V:= v; E := e0; ok := p |} => Vertices.mem n v
         end.
 
     Definition isEdge : Edges.elt -> Graph -> bool :=
       fun (e : Edges.elt) (g : Graph) =>
         match g with
-        | {| V := v; E := e0 |} => Edges.mem e e0
+        | {| V := v; E := e0; ok := p |} => Edges.mem e e0
         end.
 
     Lemma IsEmpty_reflect :
@@ -263,7 +281,7 @@ Definition destructEdge : Edge.t -> (vertex * vertex) :=
     Definition enumVertices : Graph -> Vertices.t :=
       fun (g : Graph) =>
         match g with
-        | {| V := v; E := e |} =>  v
+        | {| V := v; E := e; ok := p |} =>  v
         end.
     
     Definition enumEdges : Graph -> Edges.t :=
@@ -321,89 +339,68 @@ Definition destructEdge : Edge.t -> (vertex * vertex) :=
       simpl.
       Admitted.
       
-
+    Lemma Edge_exists2 :
+    forall G e, IsEdge e G -> IsVertex (snd (destructEdge e)) G.
+    Proof.
+      intros.
+      
 
 
 End myGraph.  
   
 
+  
+  (** Elementary Graph Operations **)
+  Parameter addVertex : vertex -> t -> t.
+  Parameter addEdge : edge -> t -> t.
+  Parameter removeVertex : vertex -> t -> t. 
+  Parameter removeEdge : edge -> t -> t.
+
+  (** Specifications of these graph operations **)
+  Parameter addVertex_spec1 :
+    forall G v, IsVertex v (addVertex v G).
+  Parameter addVertex_spec2 :
+    forall G v1 v2, v1 <v> v2 ->
+      IsVertex v1 G <-> IsVertex v1 (addVertex v2 G).
+  Parameter addVertex_spec3 :
+    forall G v e,
+      IsEdge e G <-> IsEdge e (addVertex v G).
+  Parameter addEdge_spec1 :
+    forall G e,
+      IsVertex (fst (destructEdge e)) G->
+      IsVertex (snd (destructEdge e)) G->
+        IsEdge e (addEdge e G).
+  Parameter addEdge_spec2 :
+     forall G e1 e2, e1 <e> e2 ->
+      IsEdge e1 G <-> IsEdge e1 (addEdge e2 G).
+  Parameter addEdge_spec3 : forall G v e,
+    IsVertex v G <-> IsVertex v  (addEdge e G).
+
+  Parameter removeVertex_spec1 :
+    forall G v, ~ IsVertex v (removeVertex v G).
+  Parameter removeVertex_spec2 :
+    forall G v1 v2, v1 <v> v2 ->
+      IsVertex v1 G <-> IsVertex v1 (removeVertex v2 G).
+  Parameter removeVertex_spec3 :
+    forall G v e,
+      IsEdge e (removeVertex v G) <->
+      IsEdge e G /\
+        (fst (destructEdge e)) <v> v /\
+        (snd (destructEdge e)) <v> v.
+  Parameter removeVertex_spec4 :
+    forall G v e,
+      IsEdge e G ->
+        (fst (destructEdge e) =v= v) \/
+        (snd (destructEdge e) =v= v) ->
+          ~ IsEdge e (removeVertex v G).
+
+  Parameter removeEdge_spec1 :
+    forall G e, ~ IsEdge e (removeEdge e G).
+  Parameter removeEdge_spec2 :
+    forall G e1 e2, e1 <e> e2 -> 
+      IsEdge e1 G <-> IsEdge e1 (removeEdge e2 G).
+  Parameter removeEdge_spec3 :
+    forall G v e,
+      IsVertex v G <-> IsVertex v (removeEdge e G).
 
 
-
-(*   Parameter buildEdge : elt -> elt -> elt.*)
-   Parameter destructEdge : elt -> elt * elt.
-   Parameter IsEmpty : t -> Prop.
-   Parameter IsVertex : elt -> t -> Prop.
-   Parameter IsEdge : elt -> t -> Prop.
-   Parameter isEmpty : t -> bool.
-   Parameter isVertex : elt -> t -> bool.
-   Parameter isEdge : elt -> t -> bool.
-   Parameter IsEmpty_reflect :
-     forall G : t, reflect (IsEmpty G) (isEmpty G).
-   Parameter IsVertex_reflect :
-     forall (G : elt) (v : t), reflect (IsVertex G v) (isVertex G v).
-   Parameter IsEdge_reflect :
-     forall (G : elt) (e : t), reflect (IsEdge G e) (isEdge G e).
-   Parameter enumVertices : t -> t.
-   Parameter enumEdges : t -> t.
-   Parameter Empty : IsEmpty empty.
-   Parameter Empty_vertices :
-     forall G : t, IsEmpty G -> Empty (enumVertices G).
-   Parameter Empty_edges :
-     forall G : t, IsEmpty G -> Empty (enumVertices G).
-   Parameter IsVertexEnum :
-     forall (G : t) (v : elt), IsVertex v G <-> In v (enumVertices G).
-   Parameter IsEdgeEnum :
-     forall (G : t) (e : elt), IsEdge e G <-> In e (enumEdges G).
-   Parameter Edge_exists1 :
-     forall (G : t) (e : elt),
-     IsEdge e G -> IsVertex (fst (destructEdge e)) G.
-   Parameter Edge_exists2 :
-     forall (G : t) (e : elt),
-     IsEdge e G -> IsVertex (snd (destructEdge e)) G.
-   Parameter addVertex : elt -> t -> t.
-   Parameter addEdge : elt -> t -> t.
-   Parameter removeVertex : elt -> t -> t.
-   Parameter removeEdge : elt -> t -> t.
-   Parameter addVertex_spec1 :
-     forall (G : t) (v : elt), IsVertex v (addVertex v G).
-   Parameter addVertex_spec2 :
-     forall G v1 v2 : t,
-     ~ eq v1 v2 -> IsVertex v1 G <-> IsVertex v1 (addVertex v2 G).
-   Parameter addVertex_spec3 :
-     forall (G : t) (v e : elt),
-     IsEdge e G <-> IsEdge e (addVertex v G).
-   Parameter addEdge_spec1 :
-     forall (G : t) (e : elt),
-     IsVertex (fst (destructEdge e)) G ->
-     IsVertex (snd (destructEdge e)) G -> IsEdge e (addEdge e G).
-   Parameter addEdge_spec2 :
-     forall G e1 e2 : t,
-     ~ eq e1 e2 -> IsEdge e1 G <-> IsEdge e1 (addEdge e2 G).
-   Parameter addEdge_spec3 :
-     forall (G : t) (v e : elt),
-     IsVertex v G <-> IsVertex v (addEdge e G).
-   Parameter removeVertex_spec1 :
-     forall (G : t) (v : elt), ~ IsVertex v (removeVertex v G).
-   Parameter removeVertex_spec2 :
-     forall G v1 v2 : t,
-     ~ eq v1 v2 -> IsVertex v1 G <-> IsVertex v1 (removeVertex v2 G).
-   Parameter removeVertex_spec3 :
-     forall (G : t) (v e : elt),
-     IsEdge e (removeVertex v G) <->
-     IsEdge e G /\
-     ~ eq (fst (destructEdge e)) v /\ ~ eq (snd (destructEdge e)) v.
-   Parameter removeVertex_spec4 :
-     forall (G v : t) (e : elt),
-     IsEdge e G ->
-     eq (fst (destructEdge e)) v \/ eq (snd (destructEdge e)) v ->
-     ~ IsEdge e (removeVertex v G).
-   Parameter removeEdge_spec1 :
-     forall (G : t) (e : elt), ~ IsEdge e (removeEdge e G).
-   Parameter removeEdge_spec2 :
-     forall G e1 e2 : t,
-     ~ eq e1 e2 -> IsEdge e1 G <-> IsEdge e1 (removeEdge e2 G).
-   Parameter removeEdge_spec3 :
-     forall (G : t) (v e : elt),
-     IsVertex v G <-> IsVertex v (removeEdge e G).
- End
