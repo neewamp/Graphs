ee44e820ef967096ed887ca3fbc5f3792dc0f837
ns196414 <ns196414@ohio.edu>
2017-11-15 18:39:53 -0500

    Most of the Independent stuff is done.  Some weird type class problems with lemmas about graphs not infering functions.

--- SetoidGraphs/GraphInterface.v
+++ SetoidGraphs/GraphInterface.v
@@ -1,7 +1,11 @@
+Require Import Coq.Setoids.Setoid.
 Require Import MathClasses.interfaces.abstract_algebra
         MathClasses.interfaces.finite_sets
         MathClasses.theory.finite_sets
+        MathClasses.interfaces.vectorspace
         Bool.
+
+Set Implicit Arguments.
 
 (*
 fold f i bot
@@ -9,10 +13,45 @@ fold f i bot
 f (fold f i A) a n b \in A a > b -> fold (A U a) -> 
 
 *)
-Class carrier
-      `(FullFSet vertex)
-      `(FullFSet edge) := {}.
 
+Class Mem A B := mem: A -> B -> bool.
+Class Foldable C :=
+  fold : forall {A B : Type} , (A -> B -> B) -> B -> C -> B.
+
+Class DecFullFSet {A}
+      `(FullFSet A)
+      `(Mem A (set_type A))
+      `(Foldable (set_type A))
+  : Type :=
+  {
+    mem_reflect :
+      forall (x : A) X, reflect (contains x X) (mem x X)
+  }.
+
+Section mem_spec.
+  Context `(DecFullFSet A).
+  Lemma mem_iff :
+    forall (x : A) X, (contains x X) <-> (mem x X) ≡ true.
+    intros.
+    apply reflect_iff; auto.
+    apply mem_reflect.
+  Qed.      
+End mem_spec.
+
+Section fold_specs.
+  Context {A} `(DecFullFSet A).
+  Definition for_all (f : A -> bool) (V : set_type A) :=
+    fold (fun v acc => f v && acc) true V.
+  Parameter for_all_forall : forall f V ,
+      for_all f V ≡ true <-> (forall v, contains v V -> f v ≡ true).
+End fold_specs.
+
+Class carrier
+      `(DecFullFSet vertex)
+      `(DecFullFSet edge)
+  := {}.
+
+Section Graphs.
 Class Graph
       (t : Type)
       `(carrier)
@@ -32,88 +71,90 @@ Class Graph
       (isEdge : edge -> t -> bool)
   : Type := {}.
 
-
-Class GraphProperties
-    (* `(SetContainsV : SetContains vertex) *)
-      (* `(SetContainsE : SetContains edge) *)
-      `(Graph)
-
-      (empty : t)
-      (IsEmpty_reflect : forall G:t, reflect (IsEmpty G) (isEmpty G))
-      (IsVertex_reflect : forall G v, reflect (IsVertex v G) (isVertex v G))
-  (IsEdge_reflect : forall G e, reflect (IsEdge e G) (isEdge e G))
+Class DirectedGraph
+      `(Graph) : Type :=
+  {
+      empty : t;
+      IsEmpty_reflect : forall G:t, reflect (IsEmpty G) (isEmpty G);
+      IsVertex_reflect : forall G v, reflect (IsVertex v G) (isVertex v G);
+      IsEdge_reflect : forall G e, reflect (IsEdge e G) (isEdge e G);
 
   (* t here is the type of graphs, X;t is the base type delcared in X *)
 
 
   (* Construction Restrictions *)
-  (Empty : IsEmpty empty)
-
-  (IsVertexEnum :
-     forall G v, IsVertex v G <-> contains v (enumVertices G))
-  (IsEdgeEnum :
-     forall G e, IsEdge e G <-> contains e (enumEdges G))
-  (Edge_exists1 :
-    forall G e, IsEdge e G -> IsVertex (fst (destructEdge e)) G)
-  (Edge_exists2 :
-    forall G e, IsEdge e G -> IsVertex (snd (destructEdge e)) G)
+      Empty : IsEmpty empty;
+      IsVertexEnum :
+         forall G v, IsVertex v G <-> contains v (enumVertices G);
+   IsEdgeEnum :
+     forall G e, IsEdge e G <-> contains e (enumEdges G);
+   Edge_exists1 :
+    forall G e, IsEdge e G -> IsVertex (fst (destructEdge e)) G;
+   Edge_exists2 :
+    forall G e, IsEdge e G -> IsVertex (snd (destructEdge e)) G;
   
   (* Elementary Graph Operations *)
 
   (** Specifications of these graph operations **)
-  (addVertex_spec1 :
-     forall G v, IsVertex v (addVertex v G))
-  (addVertex_spec2 :
+   addVertex_spec1 :
+     forall G v, IsVertex v (addVertex v G);
+   addVertex_spec2 :
     forall G v1 v2, Ae v1 v2 ->
-                    IsVertex v1 G <-> IsVertex v1 (addVertex v2 G))
+                    IsVertex v1 G <-> IsVertex v1 (addVertex v2 G);
 
-  (addVertex_spec3 :
+   addVertex_spec3 :
     forall G v e,
-      IsEdge e G <-> IsEdge e (addVertex v G))
+      IsEdge e G <-> IsEdge e (addVertex v G);
 
-  (addEdge_spec1 :
+   addEdge_spec1 :
     forall G e,
       IsVertex (fst (destructEdge e)) G->
       IsVertex (snd (destructEdge e)) G->
-      IsEdge e (addEdge e G))
-  (addEdge_spec2 :
+      IsEdge e (addEdge e G);
+   addEdge_spec2 :
      forall G e1 e2, Ae0 e1 e2 ->
-                     IsEdge e1 G <-> IsEdge e1 (addEdge e2 G))
-  (addEdge_spec3 : forall G v e,
-      IsVertex v G <-> IsVertex v  (addEdge e G))
-
-  (removeVertex_spec1 :
-    forall G v, ~ IsVertex v (removeVertex v G))
-  (removeVertex_spec2 :
+                     IsEdge e1 G <-> IsEdge e1 (addEdge e2 G);
+   addEdge_spec3 : forall G v e,
+      IsVertex v G <-> IsVertex v (addEdge e G);
+   removeVertex_spec1 :
+    forall G v, ~ IsVertex v (removeVertex v G);
+   removeVertex_spec2 :
     forall G v1 v2, ~ Ae v1 v2 ->
-      IsVertex v1 G <-> IsVertex v1 (removeVertex v2 G))
-  (removeVertex_spec3 :
+      IsVertex v1 G <-> IsVertex v1 (removeVertex v2 G);
+   removeVertex_spec3 :
     forall G v e,
       IsEdge e (removeVertex v G) <->
       IsEdge e G /\
         Ae (fst (destructEdge e)) v /\
-        Ae (snd (destructEdge e)) v)
-  (removeVertex_spec4 :
+        Ae (snd (destructEdge e)) v;
+   removeVertex_spec4 :
     forall G v e,
       IsEdge e G ->
       Ae (fst (destructEdge e)) v \/
       Ae (snd (destructEdge e)) v ->
-          ~ IsEdge e (removeVertex v G))
-  (removeEdge_spec1 :
-    forall G e, ~ IsEdge e (removeEdge e G))
-  (removeEdge_spec2 :
+          ~ IsEdge e (removeVertex v G);
+   removeEdge_spec1 :
+    forall G e, ~ IsEdge e (removeEdge e G);
+   removeEdge_spec2 :
     forall G (e1 e2 : edge) , Ae0 e1 e2 ->
-      IsEdge e1 G <-> IsEdge e1 (removeEdge e2 G))
-  (removeEdge_spec3 : 
+      IsEdge e1 G <-> IsEdge e1 (removeEdge e2 G);
+   removeEdge_spec3 :
     forall G v e,
-      IsVertex v G <-> IsVertex v (removeEdge e G))
-  : Type := {}.
+      IsVertex v G <-> IsVertex v (removeEdge e G)
+  }.
 
-
+Class SimpleUndirectedGraph 
+      `(DirectedGraph)
+  : Type :=
+  {
+    edges_irreflexive : forall (v : vertex) (G : t),
+      ~ IsEdge (buildEdge v v) G;
+    undirected : forall (v1 v2 : vertex) (G : t),
+        IsEdge (buildEdge v1 v2) G -> IsEdge (buildEdge v2 v1) G
+  }.
 
 Section Graphs.
-  (* Context {t} `(Graph t). *)
-  Context `(GraphProperties).
+  Context `(Graph).
   Notation "V1 =V= V2" := (conAe V1 V2) (at level 60).
   Notation "V1 <V> V2" := (~ conAe V1 V2) (at level 60). 
   Notation "v1 =v= v2" := (Ae v1 v2) (at level 60).
@@ -123,3 +164,79 @@ Section Graphs.
   Notation "e1 =e= e2" := (Ae0 e1 e2) (at level 60).
   Notation "e1 <e> e2" := (~ Ae0 e1 e2) (at level 60).
 End Graphs.
+
+
+  (* Parameter edges_irreflexive : forall (v : vertex) (G : t), *)
+  (*     ~ IsEdge (buildEdge v v) G. *)
+(* Class DirectedGraph *)
+(*       `(Graph) : Type :=  *)
+(*       (empty : t) *)
+(*       (IsEmpty_reflect : forall G:t, reflect (IsEmpty G) (isEmpty G)) *)
+(*       (IsVertex_reflect : forall G v, reflect (IsVertex v G) (isVertex v G)) *)
+(*       (IsVertex_reflect : forall G v, reflect (IsVertex v G) (isVertex v G)) *)
+(*       (IsEdge_reflect : forall G e, reflect (IsEdge e G) (isEdge e G)) *)
+
+(*   (* t here is the type of graphs, X;t is the base type delcared in X *) *)
+
+
+(*   (* Construction Restrictions *) *)
+(*       (Empty : IsEmpty empty) *)
+
+(*       (IsVertexEnum : *)
+(*          forall G v, IsVertex v G <-> contains v (enumVertices G)) *)
+(*   (IsEdgeEnum : *)
+(*      forall G e, IsEdge e G <-> contains e (enumEdges G)) *)
+(*   (Edge_exists1 : *)
+(*     forall G e, IsEdge e G -> IsVertex (fst (destructEdge e)) G) *)
+(*   (Edge_exists2 : *)
+(*     forall G e, IsEdge e G -> IsVertex (snd (destructEdge e)) G) *)
+  
+(*   (* Elementary Graph Operations *) *)
+
+(*   (** Specifications of these graph operations **) *)
+(*   (addVertex_spec1 : *)
+(*      forall G v, IsVertex v (addVertex v G)) *)
+(*   (addVertex_spec2 : *)
+(*     forall G v1 v2, Ae v1 v2 -> *)
+(*                     IsVertex v1 G <-> IsVertex v1 (addVertex v2 G)) *)
+
+(*   (addVertex_spec3 : *)
+(*     forall G v e, *)
+(*       IsEdge e G <-> IsEdge e (addVertex v G)) *)
+
+(*   (addEdge_spec1 : *)
+(*     forall G e, *)
+(*       IsVertex (fst (destructEdge e)) G-> *)
+(*       IsVertex (snd (destructEdge e)) G-> *)
+(*       IsEdge e (addEdge e G)) *)
+(*   (addEdge_spec2 : *)
+(*      forall G e1 e2, Ae0 e1 e2 -> *)
+(*                      IsEdge e1 G <-> IsEdge e1 (addEdge e2 G)) *)
+(*   (addEdge_spec3 : forall G v e, *)
+(*       IsVertex v G <-> IsVertex v (addEdge e G)) *)
+(*   (removeVertex_spec1 : *)
+(*     forall G v, ~ IsVertex v (removeVertex v G)) *)
+(*   (removeVertex_spec2 : *)
+(*     forall G v1 v2, ~ Ae v1 v2 -> *)
+(*       IsVertex v1 G <-> IsVertex v1 (removeVertex v2 G)) *)
+(*   (removeVertex_spec3 : *)
+(*     forall G v e, *)
+(*       IsEdge e (removeVertex v G) <-> *)
+(*       IsEdge e G /\ *)
+(*         Ae (fst (destructEdge e)) v /\ *)
+(*         Ae (snd (destructEdge e)) v) *)
+(*   (removeVertex_spec4 : *)
+(*     forall G v e, *)
+(*       IsEdge e G -> *)
+(*       Ae (fst (destructEdge e)) v \/ *)
+(*       Ae (snd (destructEdge e)) v -> *)
+(*           ~ IsEdge e (removeVertex v G)) *)
+(*   (removeEdge_spec1 : *)
+(*     forall G e, ~ IsEdge e (removeEdge e G)) *)
+(*   (removeEdge_spec2 : *)
+(*     forall G (e1 e2 : edge) , Ae0 e1 e2 -> *)
+(*       IsEdge e1 G <-> IsEdge e1 (removeEdge e2 G)) *)
+(*   (removeEdge_spec3 : *)
+(*     forall G v e, *)
+(*       IsVertex v G <-> IsVertex v (removeEdge e G)) *)
+(*       : Type := {}. *)
--- SetoidGraphs/IndependentSets.v
+++ SetoidGraphs/IndependentSets.v
@@ -5,24 +5,99 @@ Require Import MathClasses.implementations.list_finite_set
         MathClasses.orders.lattices
         MathClasses.orders.orders.
 Require Import MathClasses.interfaces.abstract_algebra.
+Require Import Bool.
 (*         MathClasses.interfaces.orders. *)
-Section IndependentSets.
-  Context `(GraphProperties).
+Set Implicit Arguments.
 
+Ltac solve_by_inverts n :=
+  try red;
+  match goal with | H : ?T |- _ => 
+  match type of T with Prop =>
+    solve [ 
+      inversion H; try reflexivity; eauto;
+      match n with S (S (?n')) => subst; solve_by_inverts (S n') end ]
+  end end.
+
+Ltac si :=
+  (intros; try setoid_discriminate; eauto; 
+  try match goal with
+      | H : _ /\ _ |- _ => intuition
+      | |- _ /\ _ => intuition
+      end;
+  intros; eauto;
+  try solve_by_inverts 4); eauto.
+
+Section IndependentSets.
+  Context `(Graph).
+  Definition asdf : Type.
+    
   Program  Definition IndependentSet (X : set_type vertex) (g : t) :=
     forall (x y : vertex),
       contains x X -> contains y X ->
       ~contains (buildEdge x y) (enumEdges g).
 
-  Context `(listset vertex).
-  Definition asdf : Type.
+  (* Context `(listset vertex). *)
+  (* Definition asdf : Type. *)
   
-Definition independentSet (X : set_type vertex) (g : t)
-    :=
-      fset_map id X.
+  Definition independentSet (X : set_type vertex) (G : t)
+    := for_all (fun v1 => for_all
+        (fun v2 => negb (mem (buildEdge v1 v2) (enumEdges G))) X) X.
   
+
+  Theorem independentSet_true_iff :
+    forall X G,
+  reflect (IndependentSet X G) (independentSet X G). 
+  Proof.
+    intros.
+    apply iff_reflect.
+    symmetry.
+    unfold independentSet, IndependentSet.
+    split; intros.
+    {
+      intros Hnot.
+      rewrite for_all_forall in H9.
+      apply H9 in H10.
+      rewrite for_all_forall in H10.
+      apply H10 in H11.
+      rewrite negb_true_iff in H11.
+      destruct (mem_reflect (buildEdge x y) (enumEdges G)); 
+      si.
+    }
+    repeat (rewrite for_all_forall; intros).
+    rewrite negb_true_iff.
+    destruct (mem_reflect (buildEdge v v0) (enumEdges G)); auto.
+    exfalso.
+    apply (H9 v v0); auto.
+  Qed.
+
   Definition ValidSet (X : set_type vertex) (G : t) :=
     forall (x : vertex) , contains x X -> contains x (enumVertices G).
+
+  Definition validSet (X : set_type vertex) (G : t) : bool :=
+    for_all (fun  x => if mem x X then mem x (enumVertices G) 
+            else true ) X.
+
+  Theorem validSet_true_iff :
+    forall X G, reflect (ValidSet X G) (validSet X G).
+  Proof.
+    intros.
+    apply iff_reflect.
+    symmetry.
+    unfold validSet, ValidSet.
+    rewrite for_all_forall.
+    split; intros; auto.
+    {
+      assert (x ∈ X) by auto.
+      rewrite mem_iff in H10; auto.
+      apply H9 in H11.
+      rewrite mem_iff; auto.
+      rewrite H10 in H11; auto.
+    }
+    assert (contains v X) by auto.
+    apply H9 in H10.
+    rewrite mem_iff in H10,H11; auto.
+    rewrite H11; auto.
+  Qed.
 
   Theorem validSetRecr : 
     forall x (X : set_type vertex) G,
@@ -30,7 +105,91 @@ Definition independentSet (X : set_type vertex) (g : t)
   Proof.
     red. intros.
     unfold ValidSet in *.
-    apply H4; auto.
+    apply H9; auto.
     rewrite fset_in_join.
     auto.
   Qed.
+
+
+  Inductive IndSet (X : set_type vertex) (G : t) :=
+  | defIndSet : ValidSet X G -> IndependentSet X G -> IndSet X G. 
+
+  Definition indSet (X : set_type vertex) (G : t) : bool :=
+    validSet X G && independentSet X G.
+
+  Theorem indSet_reflect : forall X G, reflect (IndSet X G) (indSet X G).
+  Proof.
+    intros.
+    apply iff_reflect.
+    unfold indSet.
+    assert (reflect (ValidSet X G) (validSet X G)).
+    apply validSet_true_iff.
+    split; try constructor; intuition.
+    destruct (independentSet_true_iff X G);
+      destruct (validSet_true_iff X G); auto;
+    inversion H10;
+    contradiction.
+    apply andb_true_iff in H10.
+    destruct H10.
+    destruct (validSet_true_iff X G); auto;
+    si.
+    destruct (independentSet_true_iff X G); auto.
+    apply andb_true_iff in H10.
+    si.
+  Qed.
+
+
+  Theorem nilIndSet : IndSet ⊥ empty.
+  Proof.
+     constructor; unfold IndependentSet;
+      try constructor; try intros x y H; unfold ValidSet; intros;
+      try apply fset_notin_empty in H9; 
+      try contradiction.    
+  Qed.
+
+  Theorem empty_IndSet_spec : forall G, IndSet bottom G.
+  Proof.
+    intros.
+    constructor.
+    unfold ValidSet.
+    intros.
+    apply fset_notin_empty in H9.
+    contradiction.
+    unfold IndependentSet.
+    intros.
+    apply fset_notin_empty in H9.
+    contradiction.
+  Qed.
+
+  Theorem nilIndSetAdd : forall x G, contains x (enumVertices G) ->
+                       IndSet (join {{x}} bottom) G.
+  Proof.
+    intros x G H10.
+    constructor.
+    {
+      unfold ValidSet.
+      intros.
+      rewrite fset_in_add in H9.
+      destruct H9.
+      rewrite H9;
+        auto.
+      apply fset_notin_empty in H9; contradiction.
+    }      
+    unfold IndependentSet.
+    intros.
+    intros Hnot.
+    rewrite fset_in_add in H9,H11.
+    destruct H9.
+    destruct H11.
+    subst.
+    admit.
+    (* eapply IsEdgeEnum  in Hnot; eauto. *)
+    (* rewrite H9 in Hnot. *)
+    (* apply edges_irreflexive in Hnot. *)
+    apply fset_notin_empty in H11; contradiction.
+    apply fset_notin_empty in H9; contradiction.
+  Admitted.
+
+
+
+
